52043c2afcec09c18d827a1bdd426331c3e012cc
Sheikh <barsamaniya11@gmail.com>
2022-09-12 19:23:13 +0500

    added detail activity

---
+++ app/src/main/java/com/sheikh/shoppinglist/presentation/screens/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.sheikh.shoppinglist.presentation.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sheikh.shoppinglist.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
