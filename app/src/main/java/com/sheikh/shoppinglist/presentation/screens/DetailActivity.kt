cd58b565d049db797a79470db3d5facf31dc1f66
Sheikh <barsamaniya11@gmail.com>
2022-09-14 23:22:51 +0500

    added shop item view model & detail act. upgraded

--- app/src/main/java/com/sheikh/shoppinglist/presentation/screens/DetailActivity.kt
+++ app/src/main/java/com/sheikh/shoppinglist/presentation/screens/DetailActivity.kt
@@ -1,12 +1,43 @@
 package com.sheikh.shoppinglist.presentation.screens
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.sheikh.shoppinglist.R
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        Log.d("intent mode",intent.getStringExtra(EXTRA_SCREEN_MODE).toString())
+    }
+
+    fun onClickSaveItem(view: View) {
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_EDIT = "mode_edit"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, shopItemID: Int): Intent {
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemID)
+            return intent
+        }
     }
 }
